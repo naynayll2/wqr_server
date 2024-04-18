7334e437fa3139402c30f4dbe31ce67f8ac53df5
naynayll2 <anyeawasum2@gmail.com>
2024-04-18 17:04:26 -0500

    completed history api, removed empty files, moved examples into folders

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express, {Express, Request, Response} from "express"
-import { MongoClient, Timestamp, Document } from "mongodb"
+import { MongoClient, Timestamp, Document, Sort, SortDirection } from "mongodb"
 import { UUID } from "bson"
-import { start } from "repl"
 
 const app = express()
 const port = process.env.PORT!!
 const URI = process.env.MONGO_URI!!
 const DB = process.env.DB!!
 const COLLECTION = process.env.COLLECTION!!
+const LAKE_COLLECTION = process.env.LAKE_NAME_COLLECTION!!
 
 const client = new MongoClient(URI, {
   pkFactory:{createPk: () => new UUID().toBinary()}
@@ -24,6 +24,7 @@ db.createCollection(COLLECTION, {
   console.log(`Failed with error ${r}`)
 })
 
+db.createCollection()
 const wqrCollection = db.collection(COLLECTION)
 
 async function batchAddToWQR(timeseries: Array<{[key: string]: any}>): Promise<boolean> {
@@ -123,20 +124,51 @@ app.get("/api/data/", async (req: Request, res: Response) => {
       })
       return accum
     }, new Array<Document>())
-    
+
     console.log(results)
     res.status(200).send(results)
-  } else if (req.body.operation === "GetAllLakeHistory") {
+  } else if (req.body.operation === "GetLakeHistory") {
+    console.log(req.body)
     const startTime = req.body.startTime
     const endTime = req.body.endTime
 
     if (startTime === undefined || endTime === undefined) {
       res.status(400).send("JSON body must have 'startTime' and 'endTime' set to valid ISO timestamps")
+    } else {
+      const query = {
+        metadata: {robotID: req.body.robotID},
+        timestamp: {
+          $gte: new Date(startTime), 
+          $lte: new Date(endTime)
+        }
+      }
+
+      const sort = { timestamp: 'desc' as SortDirection}
+
+      const results = await wqrCollection.find(query).sort(sort).toArray()
+      const finalResults = results.reduce((arr: Array<Document>, doc): Array<Document> => {
+        const newObj = Object.keys(doc).filter(key => key !== '_id').reduce((obj: Document, key: string): Document => {
+          if (key != 'metadata') {
+            obj[key] = doc[key]
+          } else {
+            obj['robotID'] = doc.metadata.robotID
+          }
+          return obj
+        }, {})
+        arr.push(newObj)
+        return arr
+      }, new Array<Document>())
+      console.log(finalResults)
+      res.status(200).send(finalResults)
     }
   } else {// No defined API, reject
+    res.status(400).send("Wrong server operation")
   }
 })
 
+app.put("/api/data/", async (req: Request, res: Response) => {
+
+})
 app.listen(port, () => {
   console.log(`Active on ${port} ${DB} ${COLLECTION}`)
 })
